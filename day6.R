200f25cadaa0f331245806a11972d8b4675d03de
exunckly <3117100+exunckly@users.noreply.github.com>
2023-12-06 11:23:05 +0000

    tweaked '+ -1' to ' - 1' for clarity

--- day6.R
+++ day6.R
@@ -19,12 +19,12 @@ library(tidyverse)
 # s = -h^2 + th - r = 0
 
 # Roots of this equation are -t + (t^2 - 4*r)^0.5)/(-2) and -t - (t^2 - 4*r)^0.5)/(-2)
-# Need to check for rounding etc when calculating the number of integers between them (inclusive)
+# Need to remember to check for rounding etc when calculating the number of integers between them (inclusive)
 
 # Maximum displacement (in case it's needed later) is halfway along the parabola
 # i.e. at t/2
-# s = 0.25 t^2
-# This may not be an integer value but you can find the nearest integer later (bearing in mind we may need to deal with 0.5)
+# smax = 0.25 t^2
+# This may not be an integer value but can find the nearest integer later (remember we may need to deal with 0.5)
 
 # Read in data
 my_data <- read_lines("Data/day6_input.txt") %>%
@@ -48,7 +48,7 @@ my_data <- my_data %>%
           r = additional_data[2], 
           source = "part2")
 
-# Answer is number of ways in which we can beat the record, i.e. the number of integers between the intercepts
+# Answer involves number of ways in which we can beat the record, i.e. the number of integers between the intercepts
 # NOT including the intercepts if they are integers because these are when we EQUAL the record
 
 my_data <- my_data %>%
@@ -58,7 +58,7 @@ my_data <- my_data %>%
   # Ceiling/floor to nearest integer unless we equalled the record on an integer
   ### Reminder to come back and check for tolerance (i.e. number of decimal places the check goes to) if I have issues
   mutate (i_1_int = ifelse (i_1 == round(i_1, 0), ceiling(i_1) + 1, ceiling(i_1))) %>%
-  mutate (i_2_int = ifelse (i_2 == round(i_2, 0), floor(i_2) + -1, floor(i_2))) %>%
+  mutate (i_2_int = ifelse (i_2 == round(i_2, 0), floor(i_2) - 1, floor(i_2))) %>%
   mutate (num_ways = abs(i_2_int - i_1_int) + 1) %>%
   # In case they are needed in part 2 :-)
   mutate (max_hold = t/2) %>%
