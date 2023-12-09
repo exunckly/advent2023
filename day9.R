4aaa7d5d87e9b7fbb6fcd511623cb21a92b98fe5
exunckly <3117100+exunckly@users.noreply.github.com>
2023-12-09 15:36:10 +0000

    Tweaked day 9 for clarity

--- day9.R
+++ day9.R
@@ -1,9 +1,10 @@
 # Advent of code 2023 - Day 9
 library(tidyverse)
 
-# Recursive function to get the answer for a sequence
-# In part 1 the next value in the sequence is sum(keep_track)
+# Recursive function to gfind next and zeroth in sequence
+# In part 1 the next value in the sequence is sum(keep_track_f) from this recursive function
 # Iterations is for debugging
+#   Post hoc note: purr::accumulate() would have been helpful
 my_extrapolate <- function(x, keep_track_f = c(), keep_track_b = c(), iterations = 0){
   # sum(x) != 0 doesn't work when we have a mix of positive and negative numbers
   if(!all(x == 0)){
@@ -20,6 +21,8 @@ my_extrapolate <- function(x, keep_track_f = c(), keep_track_b = c(), iterations
   }
 }
 
+# In part 2 the zeroth value in the sequence is slightly clunkier
+#   Post hoc note: turns out that I could just have reversed the sequences and used the same method as in part 1
 my_zeroth_val <- function(first_vals, target = 0){
   zeroth_val <- rep(NA, length(first_vals))
   for(i in rev(seq_along(first_vals))){
@@ -29,7 +32,7 @@ my_zeroth_val <- function(first_vals, target = 0){
         zeroth_val[i] <- first_vals[i] - zeroth_val[i+1]
       }
   }
-  return(zeroth_val)
+  return(zeroth_val[1])
 }
 
 # Read in data
@@ -39,15 +42,14 @@ my_data <- as_tibble(read_lines("Data/day9_input.txt")) %>%
   mutate(seq_len = length(seq_vec)) %>%
   # Keep track of first and last values in the sequences as they reduce
   mutate(extrapolation = list(my_extrapolate(unlist(seq_vec)))) %>%
-  # Calculate the next value in the sequence
-  mutate(next_val_f = sum(my_extrapolate(unlist(seq_vec))$keep_track_f)) %>%
-  mutate(iterations = my_extrapolate(unlist(seq_vec))$iterations) %>%
-  mutate(zeroth_val = my_zeroth_val(my_extrapolate(unlist(seq_vec))$keep_track_b)[1])
+  # Next value in each sequence
+  mutate(next_val = sum(extrapolation$keep_track_f)) %>%
+  mutate(iterations = extrapolation$iterations) %>%
+  # Zeroth value in each sequence
+  mutate(zeroth_val = my_zeroth_val(extrapolation$keep_track_b))
 
-part1 <- sum(my_data$next_val_f)
+part1 <- sum(my_data$next_val)
 print(part1)
-
-# 1969958987
 
 part2 <- sum(my_data$zeroth_val)
 print(part2)
